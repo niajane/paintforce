9bc970478ee91ded5d13ae24c9d2e56be1cb9497
Harry <moomoofarm123123@gmail.com>
2020-12-07 14:03:11 +0000

    Add new ColourPicker component which will be used to allow the user to change the colour of their brush

---
+++ src/components/ColourPicker/ColourPicker.jsx
@@ -0,0 +1,32 @@
+import './ColourPicker.css';
+import React from 'react';
+
+export default class ColourPicker extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { colour: '#000000' };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    render() {
+        return (
+            <div className="colour-picker-container">
+                <label htmlFor="colour-picker">
+                    Choose A Colour:
+                </label>
+                <input type="color" id="colour-picker" onChange={this.handleInputChange}></input>
+                <p className="selected-colour">{this.state.colour}</p>
+            </div>
+        );
+    }
+
+    /**
+     * This method will update the component's state with a colour when a new colour is selected on the colour picker.
+     * @param {Object} event passed when onchange event is triggered for the colour-picker input field
+     */
+    handleInputChange(event) {
+        this.setState({ colour: event.target.value });
+    }
+}
