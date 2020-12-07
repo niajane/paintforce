9926eb996531cdf589da52a52b57ec3bb907ed02
Harry <moomoofarm123123@gmail.com>
2020-12-07 12:20:35 +0000

    add css, scss and jsx for Canvas component

---
+++ src/components/canvas/Canvas.jsx
@@ -0,0 +1,12 @@
+import './Canvas.css';
+import React from 'react';
+
+export default class Canvas extends React.Component {
+    render() {
+        return (
+            <div class="canvas">
+                <div class="grid"></div>
+            </div>
+        );
+    }
+}
